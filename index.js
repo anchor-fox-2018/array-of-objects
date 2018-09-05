16e4bdaa013c79d02f6300935ee7e9530b716415
Dody Herawan <dody@Dodys-MacBook-Pro.local>
2018-09-05 13:26:37 +0700

    Release 1

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var arr = [];
+//RELEASE 0
+const arr = [];
 function createObj (name, phase, gender) {
     //code
     let tempObj = {
@@ -16,3 +17,17 @@ createObj('Tama', 2, 'Male');
 createObj('Rizky', 2, 'Male');
 
 console.log(arr);
+
+//RELEASE 1
+function getData (name) {
+    //code
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].name === name) {
+            return arr[i];
+        }
+    }
+}
+
+console.log('GET DATA || RELEASE 1')
+console.log(getData('Icha'));
+console.log(getData('Tama'));
