3406ccb5137a9e4825e7ddbd7b27c34d1b210b46
days88 <yayahidayatullah@rocketmail.com>
2018-09-05 14:21:42 +0800

    Array Of Object

---
+++ index.js
@@ -0,0 +1,19 @@
+//Release 0
+var arr = [];
+function createObj(name,phase,gender){
+    arr.push({name,phase,gender});
+}
+createObj('Akbar', 1, 'male');
+createObj('Icha', 1, 'female');
+
+console.log(arr);
+
+//Release 1
+function getData(name){
+    for(let i = 0;i < arr.length;i++){
+        if(arr[i].name === name){
+            return arr[i];
+        }
+    }
+}
+console.log(getData('Icha'));
